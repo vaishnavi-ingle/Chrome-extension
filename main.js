f240b0738d773327e82fd7c0eb094ebb7adaabb0
vaishnavi-ingle <vaishnavimingle@gmail.com>
2024-08-05 18:29:05 +0530

    js

--- main.js
+++ main.js
@@ -3,5 +3,9 @@ let inputEl = document.getElementById("input-el");
 let myLeads = [];
 
 inputBtn.addEventListener("click", function () {
-  myLeads.push("www.google.com")
+  myLeads.push(inputEl.value);
 });
+
+for (let i = 0; i < myLeads.length; i++) {
+  console.log(myLeads[i]);
+}

1aef42d6382ad5a0445046e5943a3fa0a18ea579
vaishnavi-ingle <vaishnavimingle@gmail.com>
2024-08-07 20:07:45 +0530

    New change

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let inputBtn = document.getElementById("input-btn");
 let inputEl = document.getElementById("input-el");
 let myLeads = [];
+const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);

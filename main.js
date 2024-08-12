3cd7256b144cb0890d9e505fa5f00816aba5bcf4
vaishnavi-ingle <vaishnavimingle@gmail.com>
2024-08-12 18:09:52 +0530

    new

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 let inputBtn = document.getElementById("input-btn");
 let inputEl = document.getElementById("input-el");
 let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
-const ulEl = document.getElementById("ul-el")
+const ulEl = document.getElementById("ul-el");
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
 });
 
+let listItems = " ";
 for (let i = 0; i < myLeads.length; i++) {
-  ulEl.innerHTML += "<li>" + myLeads[i] + "</li> "
+  listItems += "<li>" + myLeads[i] + "</li> ";
 }
+ulEl.innerHTML = listItems;
